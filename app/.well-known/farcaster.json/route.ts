73060a9535412e03d6e49e18472827120301023d
harshshukla9 <harshshukla9792@gmail.com>
2026-02-14 03:07:18 +0530

    assosiate

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -18,6 +18,11 @@ export async function GET() {
       splashBackgroundColor: "#ffffff",
       webhookUrl: `${APP_URL}/api/webhook`,
     },
+    accountAssociation: {
+        "header": "eyJmaWQiOjExMDg1NzQsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhkODUzOTVERWYzZDYzM0U3ODYyOTFiZjlERTU0ZDMyNGVlYkM3OTE3In0",
+        "payload": "eyJkb21haW4iOiJwb3R3YXIudmVyY2VsLmFwcCJ9",
+        "signature": "7H/fBE6mXWmsfEhXBtBiEuHJwj6YTCVJ/UkwUL6FIY8iHC7u6+q2si1WpX/5h6TCD9Fxd2VysG6CTlcvgINWGBs="
+    }
   };
 
   return NextResponse.json(farcasterConfig);
